6a1334b021ccffc0c0b8c56fca353aac35991bb6
Fabia Moroni <fabia.moroni@gmail.com>
2017-08-11 15:40:56 +0200

    authentication

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [ :index, :show]
   def index
     if params[:category]
       @category = Category.find_by_name(params[:category])

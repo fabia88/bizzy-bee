70c10552fb4592a8a1a907f18bed78e397d43897
Rebekka Haley <rebekka.haley@gmail.com>
2017-08-11 15:32:47 +0200

    Re-done seeds with read addresses and longer descriptions/bios - also still working on the fb log in so theres some migrations and files that are unfisnished

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+paragraph_extras = [" All my life I have been waiting for this opportunity. Thank you Bizzy Bee!",
+  " It's been really wonderful, and I can't wait! So exciting! Ahh!",
+  " I can't wait to get to know more people, because meeting others is my favourite thing.",
+  " My hobbies are reading, sports, DIYs and watching documantaries.",
+  " I hope that we can be friends! I'm so happy to be writing right now.",
+  " In the future I hope to improve my skills and live a well-balanced life",
+  " My current goals are to excel at my work and provide my skills to more people.",
+  " I've always enjoyed my hobbies, but now I get to share my passions with others!",
+  " Bizzy Bee is so great for sharing my skills with other people! I'm so happy!",
+  " Ahh! I feel like i'm repeating myself... but i'm just so excited!",
+  " Bizzy Bee is so amazingly helpful to connect with others!",
+  " Thanks to Bizzy Bee we're now connected and we can share our skills!"
+]
+
 # Seeds for categories ------------>
 
 names = %w(Miscellaneous Food Education Repair Clothing Animal Housework Technology Art)
@@ -22,11 +36,20 @@ photos = ["https://static.pexels.com/photos/62689/pexels-photo-62689.jpeg",
 names.each_with_index {|x, index| Category.create(name: x, photo: photos[index])}
 
 # Seeds for users ------------>
-
+bios = ["Hi! I'm Anna.",
+  "Hola~ my name is Brie.",
+  "Welcome to my bio! I'm Zoe.",
+  "Hej, I'm Valentina <3."
+]
 avatars = ["http://lorempixel.com/output/people-q-c-200-200-9.jpg",
   "http://lorempixel.com/output/people-q-c-200-200-7.jpg",
   "http://lorempixel.com/output/people-q-c-200-200-1.jpg",
   "http://lorempixel.com/output/people-q-c-200-200-6.jpg"
+]
+addresses = ["Utrechtsestraat 10HS",
+  "Wijde Heisteeg 1",
+  "Binnen Dommersstraat 15",
+  "Jan Evertsenstraat 136"
 ]
 first_names = %w(Anna Brie Zoe Valentina)
 last_names =  %w(Smith Garcia Baker Williamson)
@@ -37,7 +60,8 @@ avatars.each_with_index do |x, index|
     first_name: first_names[index],
     last_name: last_names[index],
     phone_number: Faker::PhoneNumber.phone_number,
-    address: Faker::Address.street_address,
+    address: addresses[index],
+    bio: bios[index] + paragraph_extras.sample(3).join,
     credit_card: Faker::Business.credit_card_number,
     remote_avatar_url: x
     )
@@ -68,7 +92,7 @@ descriptions = ["Kid safety guaranteed!",
 titles.each_with_index do |x, index|
   job = Job.new(
     title: x,
-    description: descriptions[index],
+    description: descriptions[index] + paragraph_extras.sample(3).join,
     rate: Faker::Number.between(5, 20),
     category: Category.find_by(name: names[index])
     )

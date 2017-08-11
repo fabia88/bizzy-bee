2caefd9fa032ecb8e6c2f00a863532cfe814b37b
Fabia Moroni <fabia.moroni@gmail.com>
2017-08-11 11:05:50 +0200

    destroy and migration

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :requests
+  has_many :requests, dependent: :destroy
   validates :title, :description, :rate, presence: true
 end

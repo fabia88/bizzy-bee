f453e49d9c20d331cb4a35aabb3e15ce2774bca8
Rebekka Haley <rebekka.haley@gmail.com>
2017-09-15 16:20:41 +0200

    Changed the footer and changed the navbar

--- app/assets/javascripts/home_scroll.js
+++ app/assets/javascripts/home_scroll.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-  $('.pages-home .navbar-bee').addClass('navbar-hidden');
+  $('.pages-home .navbar-bee').addClass('navbar-transparent');
+  $('.pages-home .navbar-bee-right').addClass('hidden');
+    $('.pages-home .navbar-bee-brand').addClass('hidden');
 
   $(window).scroll(function () {
     //if you hard code, then use console
@@ -7,10 +9,16 @@ $(document).ready(function() {
     //nav bar to stick.
     // console.log($(window).scrollTop())
     if ($(window).scrollTop() > 50) {
-      $('.pages-home .navbar-bee').removeClass('navbar-hidden');
+      $('.pages-home .navbar-bee').removeClass('navbar-transparent');
+      $('.pages-home .navbar-bee').addClass('navbar-opaque');
+      $('.pages-home .navbar-bee-right').removeClass('hidden');
+      $('.pages-home .navbar-bee-brand').removeClass('hidden');
     }
     if ($(window).scrollTop() < 51) {
-      $('.pages-home .navbar-bee').addClass('navbar-hidden');
+      $('.pages-home .navbar-bee').removeClass('navbar-opaque');
+      $('.pages-home .navbar-bee').addClass('navbar-transparent');
+      $('.pages-home .navbar-bee-right').addClass('hidden');
+      $('.pages-home .navbar-bee-brand').addClass('hidden');
     }
   });
 });
